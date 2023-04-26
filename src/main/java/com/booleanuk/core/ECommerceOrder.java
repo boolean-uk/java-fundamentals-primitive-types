9b67961914f6a77cc15c1ee5d84567b93d7cce42
Nikolaos Giagkinis <giagkinis.nick@gmail.com>
2023-04-26 15:09:05 +0300

    Extension second half

---
+++ src/main/java/com/booleanuk/core/ECommerceOrder.java
@@ -0,0 +1,10 @@
+package com.booleanuk.core;
+
+public class ECommerceOrder {
+    public int itemId = 5;
+    public String itemName = "Oranges";
+    public float price = 0.5f;
+    public int quantity = 10;
+    public int costumerId = 1;
+    public String paymentType = "card";
+}
