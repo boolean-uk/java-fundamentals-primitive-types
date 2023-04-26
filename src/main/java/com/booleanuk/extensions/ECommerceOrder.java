3ce18f5f20dfb099278640d0c74688ee15794994
Aidan van Geest <aidanvangeest@gmail.com>
2023-04-26 15:46:11 +0200

    Extensions completed

---
+++ src/main/java/com/booleanuk/extensions/ECommerceOrder.java
@@ -0,0 +1,10 @@
+package com.booleanuk.extensions;
+
+public class ECommerceOrder {
+    public String product = "Fitbit Inspire 3";
+    public String price = "€89";
+    public String shippingAddress = "123 Road, Amsterdam";
+    public int trackingID = 3128763;
+    public String paymentMethod = "Credit card";
+    public String deliveryDate = "28.04.2023";
+}
