c349ea90224b328b2f0de88d3313d84b0f496675
Rose Breedveld <rosebreedveld@hotmail.com>
2023-04-26 09:25:29 -0400

    extension book - social media

---
+++ src/main/java/com/booleanuk/extensions/Computer.java
@@ -0,0 +1,13 @@
+package com.booleanuk.extensions;
+
+public class Computer {
+    public String type = "Mac-Book Pro";
+    public String brandName = "Apple";
+    public String price = "€ 3049.00";
+    public String colour = "Space-Grey";
+    public String processor = "M2 pro";
+    public int year = 2023;
+    public String storage = "512 GB SSD";
+    public String screenSize = "16-inch";
+
+}
