499c51377afaaa29010f62903dd077f390a4d832
PiotrSadolewski <piotr.sadolewski00@gmail.com>
2023-07-04 13:00:58 +0200

    eight exercise

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -32,11 +32,11 @@ public class Exercise extends ExerciseBase {
 
 
     // 7. Create a public char member named lastLetter containing the last letter of the English alphabet
-    char lastLetter = 'Z';
+    public char lastLetter = 'Z';
 
 
     // 8. Create a public float member named pi that contains the value of pi to two decimal places
-
+    public float pi = 3.14f;
 
 
     // 9. Create a public double member named piD that contains the value of pi to 5 decimal places
