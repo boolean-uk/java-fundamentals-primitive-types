32a9482238e062195cf234deede5470bc7305c88
iliask796 <iliaskal7@gmail.com>
2023-08-30 16:27:03 +0300

    Finished Core (Made some extension classes even though not needed)

---
+++ src/main/java/com/booleanuk/extension/Book.java
@@ -0,0 +1,9 @@
+package com.booleanuk.extension;
+
+public class Book {
+    public String title;
+    public int publishYear;
+    public float price;
+    public String isbn;
+    public String author;
+}
